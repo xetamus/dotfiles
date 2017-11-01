7966649edc6b89fa21b6eb87aafcf94a5aa78b65
xetamus <shishamin@gmail.com>
2017-11-01 14:59:50 +0000

    use env to locate zsh for hyper

--- setup.sh
+++ setup.sh
@@ -4,15 +4,16 @@
 wget -O /tmp/hyper.deb https://releases.hyper.is/download/deb
 sudo dpkg -i /tmp/hyper.deb
 
-# Copy fonts
-mkdir -p ~/.fonts
-cp -r ~/dotfiles/fonts/* ~/.fonts
+git clone https://github.com/xetamus/dotfiles ~/dotfiles
 
 # Copy some dotfiles
-git clone https://github.com/xetamus/dotfiles ~/dotfiles
 cp ~/dotfiles/.zshrc ~/dotfiles/.hyper.js ~/dotfiles/.tmux.conf ~/
 mkdir -p ~/.config/i3
 cp -r ~/dotfiles/.config/i3/basic_config ~/.config/i3/config
+
+# Copy fonts
+mkdir -p ~/.fonts
+cp -r ~/dotfiles/fonts/* ~/.fonts
 
 # Clean up
 rm -rf ~/dotfiles
